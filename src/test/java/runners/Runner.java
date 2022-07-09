4834b4ff1ede9f1f8d326cde3e962e9f0a2e8788
SZGN <szgnmhmmd@gmail.com>
2022-07-10 00:55:28 +0300

    Hotel Project Start

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,35 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+
+@CucumberOptions(
+        plugin = {"pretty",
+                "html:target/cucumber-reports/cucumber.html",
+                "json:target/json-reports/cucumber.json",
+                "testng:target/xml-report/cucumber.xml"
+        },
+        features = "src/test/resources/features",
+        glue = "stepDefinitions",
+        tags = "(@cs1)",
+        dryRun = false
+)
+
+public class Runner extends AbstractTestNGCucumberTests {
+
+
+    @Override
+    @DataProvider(parallel = false)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+
+
+
+
+    }
+
+
+
---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,65 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.time.Duration;
+
+public class Driver {
+
+    private Driver() {
+
+    }
+
+    static WebDriver driver;
+
+    public static WebDriver getDriver() {
+
+        if (driver == null) {
+            switch (ConfigReader.getProperty("browser")) {
+                case "safari" -> {
+                    WebDriverManager.edgedriver().setup();
+                    driver = new SafariDriver();
+                }
+                case "firefox" -> {
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                }
+                case "headless-chrome" -> {
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
+                }
+                default -> {
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                }
+            }
+
+
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        }
+        return driver;
+    }
+
+    public static void closeDriver() {
+        if (driver != null) {
+            driver.close();
+            driver = null;
+        }
+
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+
+
+    }
+}
