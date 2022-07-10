215bc97a12f0beaa383ef515a80aec33ae868564
SZGN <szgnmhmmd@gmail.com>
2022-07-10 14:17:00 +0300

    07/10 pages Update

--- src/test/java/pages/RestaurantPage.java
+++ src/test/java/pages/RestaurantPage.java
@@ -1,4 +1,16 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class RestaurantPage {
+
+    @FindBy (xpath = "//*[@class='ml-md-0']")
+    public WebElement ourRestorantsText;
+
+    @FindBy (xpath = "(//*[@class='container']//h2)[2]")
+    public WebElement ourMenuText;
+
+    @FindBy (xpath = "//div[@class='desc pl-3']")
+    public WebElement MenuItems;
 }
